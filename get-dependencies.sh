3b3ca748a5b479fc793d496b5808ea30225e9859
juju4 <juju4@users.noreply.github.com>
2016-10-30 15:47:42 -0400

    missing another dep 2

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,5 +10,6 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
+[ ! -d $rolesdir/juju4.fprobe ] && git clone https://github.com/juju4/ansible-fprobe $rolesdir/juju4.fprobe
 [ ! -d $rolesdir/geerlingguy.mysql ] && git clone https://github.com/geerlingguy/ansible-role-mysql.git $rolesdir/geerlingguy.mysql
 
