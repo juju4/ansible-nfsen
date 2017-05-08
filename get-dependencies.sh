ac71327e71b2a4f1aeb7dbb275c26ce34bb80976
juju4 <juju4@users.noreply.github.com>
2017-05-08 19:01:59 -0400

    redhat-epel role dependency

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,6 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 [ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
 [ ! -d $rolesdir/juju4.fprobe ] && git clone https://github.com/juju4/ansible-fprobe $rolesdir/juju4.fprobe
 [ ! -d $rolesdir/geerlingguy.mysql ] && git clone https://github.com/geerlingguy/ansible-role-mysql.git $rolesdir/geerlingguy.mysql
